5b7a4bcc290affca44a2a615774ba63ec535c605
Soumyabrata Moitra <soumya.moitra13@gmail.com>
2024-11-26 15:56:19 +0530

    Enabling the scheduler

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from app.scheduler import start_scheduler
 app = create_app()
 
 if __name__ == "__main__":
-    #scheduler = start_scheduler(app)
+    scheduler = start_scheduler(app)
     app.run(debug=True)
